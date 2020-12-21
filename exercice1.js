ad8c154456ef5b8ae3e6650df6d857dce88434ef
ChloeArd <chloe.ardoise@gmail.com>
2020-12-21 16:00:38 +0100

    Exercice 99

--- exercice1.js
+++ exercice1.js
@@ -1,11 +1,20 @@
 // Make it count up on a timer, calling this function
 let seconds = document.getElementById("seconds");
+let decompte = 0;
+let timeoutID;
 let countUp = function() {
-    // ?
+    timeoutID = setTimeout(function () {
+        if(decompte <= 60){
+            seconds.innerHTML = decompte;
+            decompte++;
+            countUp();
+        }
+    }, 1000)
 };
 
 // How can you make it stop counting?
 let stopCountUp = function() {
+    clearTimeout(timeoutID);
 };
 let stopButton = document.getElementById("stop-button");
 stopButton.addEventListener("click", stopCountUp);
